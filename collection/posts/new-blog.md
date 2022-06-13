8563f06897d8ab2a16b1d18e1da46e95a3069cc4
brob <bryanlrobinson@gmail.com>
2022-06-13 09:58:11 -0400

    Updates collection example and render example

--- collection/posts/new-blog.md
+++ collection/posts/new-blog.md
@@ -1,5 +1,6 @@
 ---
 title: "New Blog"
+tags: "posts"
 ---
 
 
--- render/.eleventy.js
+++ render/.eleventy.js
@@ -1,4 +1,5 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(EleventyRenderPlugin);
 }
